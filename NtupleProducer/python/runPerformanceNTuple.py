5a86ac94d12b909f5bbe28669f34682976fa151f
Christopher Brown <c.brown19@imperial.ac.uk>
2024-11-14 10:12:13 +0100

    Remove input file workaround

--- NtupleProducer/python/runPerformanceNTuple.py
+++ NtupleProducer/python/runPerformanceNTuple.py
@@ -843,4 +843,3 @@ if True:
         getattr(process, 'l1tLayer1'+R).pfAlgoParameters.debug = True
 
 # open("debugDumpJetNTuple.py", "w").write(process.dumpPython())
-process.source.fileNames = ["file:inputs131X_1.root"]
